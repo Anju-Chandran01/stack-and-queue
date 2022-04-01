950fb1d840ebdb6cf153bc9f3b36197949004f76
Anju-Chandran01 <anjuchandrancs01@gmail.com>
2022-04-01 03:42:50 -0700

    Added : Peek and Pop from stack

--- src/com/bridgelabz/stackandqueue/StackLinkedList.java
+++ src/com/bridgelabz/stackandqueue/StackLinkedList.java
@@ -3,24 +3,48 @@ package com.bridgelabz.stackandqueue;
 import static java.lang.System.exit;
 
 public class StackLinkedList {
-    private static class Node {
 
+    private static class Node {
         int data;
         Node link;
     }
 
     Node top;
-
     StackLinkedList() {
         this.top = null;
     }
 
     public void push(int x) {
         Node temp = new Node();
-
         temp.data = x;
         temp.link = top;
         top = temp;
+    }
+
+    // check empty function
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    // return top element in a stack
+    public int peek() {
+        if (!isEmpty()) {
+            return top.data;
+        } else {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+    }
+
+
+
+    // pop top element from the stack
+    public void pop() {
+        if (top == null) {
+            System.out.print("\nStack Underflow");
+            return;
+        }
+        top = (top).link;
     }
 
     public void display() {
@@ -30,7 +54,7 @@ public class StackLinkedList {
         } else {
             Node temp = top;
             while (temp != null) {
-                System.out.println( temp.data);
+                System.out.println(temp.data);
                 temp = temp.link;
             }
         }
